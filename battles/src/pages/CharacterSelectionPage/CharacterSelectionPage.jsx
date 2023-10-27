44f631cd34c41c2ea82eca7d4d539b30b69cc404
paulagimeno <paula-gl-98@hotmail.com>
2023-10-27 12:28:08 +0200

    printed characters

--- battles/src/pages/CharacterSelectionPage/CharacterSelectionPage.jsx
+++ battles/src/pages/CharacterSelectionPage/CharacterSelectionPage.jsx
@@ -1,10 +1,25 @@
 import axios from 'axios';
+import CharactersGallery from '../../components/CharactersGallery/CharactersGallery';
+import { useEffect, useState } from 'react';
 
 export default function CharacterSelectionPage(){
 
-    
+    const[characters, setCharacters] = useState([])
 
-    return(
+   
+    useEffect(() =>{
+        const getCharacters = async() =>{
+            const {data} = await axios('http://localhost:5051/characters/characters')
+            setCharacters(data)
+            
+        }
+        
+        getCharacters();
+    }, [])
+
+    return(<div>
         <h1>HOLITA</h1>
+        <CharactersGallery data={characters}/>
+        </div>
     )
 }
