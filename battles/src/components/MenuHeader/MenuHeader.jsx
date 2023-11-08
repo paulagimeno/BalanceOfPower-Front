4e6cfea0708974a6fccf7d83dbb4c0815417c2c6
paulagimeno <paula-gl-98@hotmail.com>
2023-11-08 10:43:09 +0100

    pointer

--- battles/src/components/MenuHeader/MenuHeader.jsx
+++ battles/src/components/MenuHeader/MenuHeader.jsx
@@ -11,7 +11,7 @@ export default function MenuHeader() {
         <div className='menuHeader'>
             <div className='volver' onClick={goBack}>
                 <img className='flecha' src='https://res.cloudinary.com/dvmkyxyc0/image/upload/v1697845997/Vector_jvcokm.png' alt='flechita'></img>
-                <p>Back</p>
+                <p className='back'>Back</p>
             </div>
             <div className='homeLink'>
                 <Link to='/'>
